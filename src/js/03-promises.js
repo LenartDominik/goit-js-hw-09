eff7006016b38ead38005b33b5a1b23bccef76a5
Dominik Lenart <dominik.lenart84@gmail.com>
2023-01-19 17:14:46 +0100

    update - task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,38 @@ const stepInput = form.querySelector('[name="step"]');
 const amountInput = form.querySelector('[name="amount"]');
 const submitBtn = form.querySelector('[type="submit"]');
 
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      console.log('ShouldResolve:', shouldResolve);
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+  let firstDelay = parseInt(delayInput.value);
+  let step = parseInt(stepInput.value);
+  let amount = parseInt(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, firstDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    firstDelay += step;
+  }
+});
