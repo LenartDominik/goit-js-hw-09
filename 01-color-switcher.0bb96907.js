d6836eaae85f4ac1e21c3093900edc8b2f7394cf
LenartDominik <111123528+LenartDominik@users.noreply.github.com>
2023-01-19 22:06:05 +0000

    Deploying to gh-pages from @ LenartDominik/goit-js-hw-09@957564af3bab499b382bc799e6c0bec2e0d79f2c 🚀

---
+++ 01-color-switcher.0bb96907.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=null;t.disabled=!0;e.addEventListener("click",(function(){d=setInterval((function(){document.querySelector("body").style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0bb96907.js.map
