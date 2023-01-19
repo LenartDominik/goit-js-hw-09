2c09d138e37e9ec492c899684e8addd946e78d47
Dominik Lenart <dominik.lenart84@gmail.com>
2023-01-19 17:06:29 +0100

    update - task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,8 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('form');
+const delayInput = form.querySelector('[name="delay"]');
+const stepInput = form.querySelector('[name="step"]');
+const amountInput = form.querySelector('[name="amount"]');
+const submitBtn = form.querySelector('[type="submit"]');
+
