7c6a90f52585e0f9b65f6dad4d65452763585294
Dominik Lenart <dominik.lenart84@gmail.com>
2023-01-19 15:58:35 +0100

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const bodyHtml = document.querySelector('body');
 
+let timerChangeColor = null;
+
+buttonStart.addEventListener('click', () => {
+  timerChangeColor = setInterval(() => {
+    bodyHtml.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.setAttribute('disabled', '');
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerChangeColor);
+  buttonStart.removeAttribute('disabled', '');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
