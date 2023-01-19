fe10071adb8e7ba55653a6a543bb432c87fb466d
Dominik Lenart <dominik.lenart84@gmail.com>
2023-01-19 16:39:42 +0100

    task 2- timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,72 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+
+const calendar = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysRemaining = document.querySelector('[data-days]');
+const hoursRemaining = document.querySelector('[data-hours]');
+const minutesRemaining = document.querySelector('[data-minutes]');
+const secondsRemaining = document.querySelector('[data-seconds]');
+startBtn.disabled = true;
+
+//conversion function
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  //remaining time
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+let getSelectedDate;
+
+// options for flatpickr
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date().getTime()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      getSelectedDate = selectedDates[0].getTime();
+    }
+  },
+};
+flatpickr(calendar, options);
+
+//leading 0
+const leadingZero = e => {
+  return String(e).padStart(2, '0');
+};
+// countDown function
+const countDown = () => {
+  setInterval(function () {
+    const remainingTime = getSelectedDate - new Date().getTime();
+    const conversion = convertMs(remainingTime);
+
+    if (remainingTime <= 0) {
+      clearInterval(countDown);
+      startBtn.disabled = false;
+    } else {
+      startBtn.disabled = true;
+      daysRemaining.textContent = leadingZero(conversion.days);
+      hoursRemaining.textContent = leadingZero(conversion.hours);
+      minutesRemaining.textContent = leadingZero(conversion.minutes);
+      secondsRemaining.textContent = leadingZero(conversion.seconds);
+    }
+  }, 1000);
+};
+
+startBtn.addEventListener('click', countDown);
